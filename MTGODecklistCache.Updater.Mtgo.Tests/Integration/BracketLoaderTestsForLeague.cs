48ba7a5db3879b43264188319896cce55348b142
Filipe Badaró <badaro@gmail.com>
2023-07-25 16:23:03 -0300

    Moved Tools code from MTGODecklistCache to here

---
+++ MTGODecklistCache.Updater.Mtgo.Tests/Integration/BracketLoaderTestsForLeague.cs
@@ -0,0 +1,24 @@
+﻿using MTGODecklistCache.Updater.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTGODecklistCache.Updater.Mtgo.Tests
+{
+    class BracketLoaderTestsForLeague : BracketLoaderTests
+    {
+        public BracketLoaderTestsForLeague()
+        {
+        }
+
+        protected override Round[] GetBracket()
+        {
+            return null;
+        }
+
+        protected override Uri GetEventUri()
+        {
+            return new Uri("https://www.mtgo.com/en/mtgo/decklist/modern-league-2020-08-04");
+        }
+    }
+}
---
+++ MTGODecklistCache.Updater.Mtgo.Tests/Integration/BracketLoaderTestsForPrelim.cs
@@ -0,0 +1,24 @@
+﻿using MTGODecklistCache.Updater.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTGODecklistCache.Updater.Mtgo.Tests
+{
+    class BracketLoaderTestsForPrelim : BracketLoaderTests
+    {
+        public BracketLoaderTestsForPrelim()
+        {
+        }
+
+        protected override Round[] GetBracket()
+        {
+            return null;
+        }
+
+        protected override Uri GetEventUri()
+        {
+            return new Uri("https://www.mtgo.com/en/mtgo/decklist/modern-preliminary-2022-10-2512488091");
+        }
+    }
+}
