e3e31de37dc74cce245fa8c53d32aaf8f68582da
Filipe Badaró <badaro@gmail.com>
2023-10-18 20:11:37 -0300

    Minor normalizationj fixes to melee client

--- MTGODecklistCache.Updater.MtgMelee.Client/MtgMeleeClient.cs
+++ MTGODecklistCache.Updater.MtgMelee.Client/MtgMeleeClient.cs
@@ -28,37 +28,17 @@ namespace MTGODecklistCache.Updater.MtgMelee.Client
 
             var tournamentInfoDiv = doc.DocumentNode.SelectSingleNode("//div[@id='tournament-headline-details-card']");
             var tournamentName = WebUtility.HtmlDecode(tournamentInfoDiv.SelectSingleNode("a/h3").InnerText.Trim());
-            result.Name = tournamentName;
+            result.Name = NormalizeSpaces(tournamentName);
 
             var tournamentOrganizerLink = doc.DocumentNode.SelectSingleNode("//p[@id='tournament-headline-organization']/a");
             var tournamentOrganizer = WebUtility.HtmlDecode(tournamentOrganizerLink.InnerText.Trim());
-            result.Organizer = tournamentOrganizer;
+            result.Organizer = NormalizeSpaces(tournamentOrganizer);
 
             var tournamentDate = doc.DocumentNode.SelectSingleNode("//p[@id='tournament-headline-start-date-field']/span").Attributes["data-value"].Value.Trim();
             result.Date = DateTime.Parse(tournamentDate, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal).ToUniversalTime();
 
             var tournamentRegistration = doc.DocumentNode.SelectSingleNode("//p[@id='tournament-headline-registration']").InnerText.Trim();
             result.Formats = tournamentRegistration.Split("|").FirstOrDefault(f => f.Contains("Format:")).Replace("Format:", "").Trim().Split(",").Select(f => f.Trim()).ToArray();
-
-            var phaseNode = doc.DocumentNode.SelectNodes("//div[@id='standings-phase-selector-container']")?.First();
-
-            if (phaseNode != null)
-            {
-                var phaseId = phaseNode.SelectNodes("button[@class='btn btn-primary round-selector']").Last().Attributes["data-id"].Value;
-
-                string phaseParameters = MtgMeleeConstants.PhaseParameters.Replace("{start}", "0");
-                string phaseUrl = MtgMeleeConstants.PhasePage.Replace("{phaseId}", phaseId);
-
-                string json = Encoding.UTF8.GetString(new WebClient().UploadValues(phaseUrl, "POST", HttpUtility.ParseQueryString(phaseParameters)));
-                var phase = JsonConvert.DeserializeObject<dynamic>(json);
-
-                int maxDecklists = 0;
-                foreach (var player in phase.data)
-                {
-                    int playerDecklists = player.Decklists.Count;
-                    if (playerDecklists > maxDecklists) maxDecklists = playerDecklists;
-                }
-            }
 
             return result;
         }
