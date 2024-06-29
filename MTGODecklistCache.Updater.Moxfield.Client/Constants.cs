b7c721ec882cd9f244f135a953b9e252abdd3219
Filipe Badaró <badaro@gmail.com>
2024-06-29 16:12:07 -0300

    Implemented client for MoxField

---
+++ MTGODecklistCache.Updater.Moxfield.Client/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTGODecklistCache.Updater.Moxfield.Client
+{
+    public static class Constants
+    {
+        public static readonly string DeckPage = "https://www.moxfield.com/decks/{deckId}";
+        public static readonly string DeckApiPage = "https://api2.moxfield.com/v3/decks/all/{deckId}";
+    }
+}
---
+++ MTGODecklistCache.Updater.Moxfield.Client/MoxFieldClient.cs
@@ -0,0 +1,49 @@
+﻿using MTGODecklistCache.Updater.Model;
+using MTGODecklistCache.Updater.Tools;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace MTGODecklistCache.Updater.Moxfield.Client
+{
+    public class MoxFieldClient
+    {
+        public Deck GetDeck(string deckId)
+        {
+            var json = new WebClient().DownloadString(Constants.DeckApiPage.Replace("{deckId}", deckId));
+            dynamic deck = JsonConvert.DeserializeObject<dynamic>(json);
+
+            List<DeckItem> maindeckCards = new List<DeckItem>();
+            foreach (var card in deck.boards.mainboard.cards)
+            {
+                string name = card.First.card.name;
+                int count = card.First.quantity;
+
+                maindeckCards.Add(new DeckItem()
+                {
+                    CardName = CardNameNormalizer.Normalize(name),
+                    Count = count
+                }); ;
+            }
+
+            List<DeckItem> sideboardCards = new List<DeckItem>();
+            foreach (var card in deck.boards.sideboard.cards)
+            {
+                string name = card.First.card.name;
+                int count = card.First.quantity;
+
+                sideboardCards.Add(new DeckItem()
+                {
+                    CardName = CardNameNormalizer.Normalize(name),
+                    Count = count
+                });
+            }
+
+            return new Deck()
+            {
+                AnchorUri = new Uri(Constants.DeckPage.Replace("{deckId}", deckId)),
+                Mainboard = maindeckCards.ToArray(),
+                Sideboard = sideboardCards.ToArray()
+            };
+        }
+    }
+}
