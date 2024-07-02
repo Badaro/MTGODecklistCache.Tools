61ce89c1fab32e134c742649221e358bc48aeb1a
Filipe Badaró <badaro@gmail.com>
2024-07-02 09:12:31 -0300

    Added support and tests to read decklist snapshot from topdeck

---
+++ MTGODecklistCache.Updater.Topdeck.Client/Model/TopdeckListTournamentDeckSnapshot.cs
@@ -0,0 +1,17 @@
+﻿using MTGODecklistCache.Updater.Topdeck.Client.Constants;
+using Newtonsoft.Json;
+
+namespace MTGODecklistCache.Updater.Topdeck.Client.Model
+{
+    public class TopdeckListTournamentDeckSnapshot : NormalizableObject
+    {
+        [JsonProperty("mainboard")]
+        public Dictionary<string, int>? Mainboard { get; set; }
+        [JsonProperty("sideboard")]
+        public Dictionary<string,int>? Sideboard { get; set; }
+
+        public void Normalize()
+        {
+        }
+    }
+}
