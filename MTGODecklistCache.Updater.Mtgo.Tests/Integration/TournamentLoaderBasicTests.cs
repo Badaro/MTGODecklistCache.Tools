c46bb7e6fb36c69ab18acb43b4c66f28f8ec5334
Filipe Badaró <badaro@gmail.com>
2024-07-08 16:05:11 -0300

    Fixed mtgo tournament loader tests by removing prelims

--- MTGODecklistCache.Updater.Mtgo.Tests/Integration/TournamentLoaderBasicTests.cs
+++ MTGODecklistCache.Updater.Mtgo.Tests/Integration/TournamentLoaderBasicTests.cs
@@ -20,7 +20,7 @@ namespace MTGODecklistCache.Updater.Mtgo.Tests
         [Test]
         public void TournamentCountIsCorrect()
         {
-            _testData.Length.Should().Be(18);
+            _testData.Length.Should().Be(15);
         }
 
         [Test]
@@ -30,15 +30,12 @@ namespace MTGODecklistCache.Updater.Mtgo.Tests
             {
                 new Tournament(){ Date=new DateTime(2020, 06, 02, 00, 00, 00, DateTimeKind.Utc), Name="Limited Super Qualifier", Uri=new Uri("https://www.mtgo.com/decklist/limited-super-qualifier-2020-06-0212162899"), JsonFile="limited-super-qualifier-2020-06-0212162899.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 02, 00, 00, 00, DateTimeKind.Utc), Name="Limited Super Qualifier", Uri=new Uri("https://www.mtgo.com/decklist/limited-super-qualifier-2020-06-0212162898"), JsonFile="limited-super-qualifier-2020-06-0212162898.json" },
-                new Tournament(){ Date=new DateTime(2020, 06, 02, 00, 00, 00, DateTimeKind.Utc), Name="Modern Preliminary",      Uri=new Uri("https://www.mtgo.com/decklist/modern-preliminary-2020-06-0212162954"),      JsonFile="modern-preliminary-2020-06-0212162954.json" },
-                new Tournament(){ Date=new DateTime(2020, 06, 02, 00, 00, 00, DateTimeKind.Utc), Name="Pioneer Preliminary",     Uri=new Uri("https://www.mtgo.com/decklist/pioneer-preliminary-2020-06-0212162951"),     JsonFile="pioneer-preliminary-2020-06-0212162951.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 02, 00, 00, 00, DateTimeKind.Utc), Name="Modern League",           Uri=new Uri("https://www.mtgo.com/decklist/modern-league-2020-06-025082"),               JsonFile="modern-league-2020-06-025082.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 02, 00, 00, 00, DateTimeKind.Utc), Name="Pauper League",           Uri=new Uri("https://www.mtgo.com/decklist/pauper-league-2020-06-025090"),               JsonFile="pauper-league-2020-06-025090.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 02, 00, 00, 00, DateTimeKind.Utc), Name="Pioneer League",          Uri=new Uri("https://www.mtgo.com/decklist/pioneer-league-2020-06-025098"),              JsonFile="pioneer-league-2020-06-025098.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 02, 00, 00, 00, DateTimeKind.Utc), Name="Standard League",         Uri=new Uri("https://www.mtgo.com/decklist/standard-league-2020-06-025106"),             JsonFile="standard-league-2020-06-025106.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 02, 00, 00, 00, DateTimeKind.Utc), Name="Vintage League",          Uri=new Uri("https://www.mtgo.com/decklist/vintage-league-2020-06-025182"),              JsonFile="vintage-league-2020-06-025182.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 02, 00, 00, 00, DateTimeKind.Utc), Name="Legacy League",           Uri=new Uri("https://www.mtgo.com/decklist/legacy-league-2020-06-025190"),               JsonFile="legacy-league-2020-06-025190.json" },
-                new Tournament(){ Date=new DateTime(2020, 06, 01, 00, 00, 00, DateTimeKind.Utc), Name="Modern Preliminary",      Uri=new Uri("https://www.mtgo.com/decklist/modern-preliminary-2020-06-0112162949"),      JsonFile="modern-preliminary-2020-06-0112162949.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 01, 00, 00, 00, DateTimeKind.Utc), Name="Modern Super Qualifier",  Uri=new Uri("https://www.mtgo.com/decklist/modern-super-qualifier-2020-06-0112162897"),  JsonFile="modern-super-qualifier-2020-06-0112162897.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 01, 00, 00, 00, DateTimeKind.Utc), Name="Modern League",           Uri=new Uri("https://www.mtgo.com/decklist/modern-league-2020-06-015082"),               JsonFile="modern-league-2020-06-015082.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 01, 00, 00, 00, DateTimeKind.Utc), Name="Pauper League",           Uri=new Uri("https://www.mtgo.com/decklist/pauper-league-2020-06-015090"),               JsonFile="pauper-league-2020-06-015090.json" },
--- MTGODecklistCache.Updater.Mtgo.Tests/Integration/TournamentLoaderCrossMonthTests.cs
+++ MTGODecklistCache.Updater.Mtgo.Tests/Integration/TournamentLoaderCrossMonthTests.cs
@@ -20,7 +20,7 @@ namespace MTGODecklistCache.Updater.Mtgo.Tests
         [Test]
         public void TournamentCountIsCorrect()
         {
-            _testData.Length.Should().Be(21);
+            _testData.Length.Should().Be(19);
         }
 
         [Test]
@@ -28,7 +28,6 @@ namespace MTGODecklistCache.Updater.Mtgo.Tests
         {
             _testData.Should().BeEquivalentTo(new Tournament[]
             {
-                new Tournament(){ Date=new DateTime(2020, 06, 01, 00, 00, 00, DateTimeKind.Utc), Name="Modern Preliminary",      Uri=new Uri("https://www.mtgo.com/decklist/modern-preliminary-2020-06-0112162949"),      JsonFile="modern-preliminary-2020-06-0112162949.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 01, 00, 00, 00, DateTimeKind.Utc), Name="Modern Super Qualifier",  Uri=new Uri("https://www.mtgo.com/decklist/modern-super-qualifier-2020-06-0112162897"),  JsonFile="modern-super-qualifier-2020-06-0112162897.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 01, 00, 00, 00, DateTimeKind.Utc), Name="Modern League",           Uri=new Uri("https://www.mtgo.com/decklist/modern-league-2020-06-015082"),               JsonFile="modern-league-2020-06-015082.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 01, 00, 00, 00, DateTimeKind.Utc), Name="Pauper League",           Uri=new Uri("https://www.mtgo.com/decklist/pauper-league-2020-06-015090"),               JsonFile="pauper-league-2020-06-015090.json" },
@@ -36,7 +35,6 @@ namespace MTGODecklistCache.Updater.Mtgo.Tests
                 new Tournament(){ Date=new DateTime(2020, 06, 01, 00, 00, 00, DateTimeKind.Utc), Name="Standard League",         Uri=new Uri("https://www.mtgo.com/decklist/standard-league-2020-06-015106"),             JsonFile="standard-league-2020-06-015106.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 01, 00, 00, 00, DateTimeKind.Utc), Name="Vintage League",          Uri=new Uri("https://www.mtgo.com/decklist/vintage-league-2020-06-015182"),              JsonFile="vintage-league-2020-06-015182.json" },
                 new Tournament(){ Date=new DateTime(2020, 06, 01, 00, 00, 00, DateTimeKind.Utc), Name="Legacy League",           Uri=new Uri("https://www.mtgo.com/decklist/legacy-league-2020-06-015190"),               JsonFile="legacy-league-2020-06-015190.json" },
-                new Tournament(){ Date=new DateTime(2020, 05, 31, 00, 00, 00, DateTimeKind.Utc), Name="Vintage Preliminary",     Uri=new Uri("https://www.mtgo.com/decklist/vintage-preliminary-2020-05-3112162942"),     JsonFile="vintage-preliminary-2020-05-3112162942.json" },
                 new Tournament(){ Date=new DateTime(2020, 05, 31, 00, 00, 00, DateTimeKind.Utc), Name="Standard Challenge",      Uri=new Uri("https://www.mtgo.com/decklist/standard-challenge-2020-05-3112162941"),      JsonFile="standard-challenge-2020-05-3112162941.json" },
                 new Tournament(){ Date=new DateTime(2020, 05, 31, 00, 00, 00, DateTimeKind.Utc), Name="Pauper Challenge",        Uri=new Uri("https://www.mtgo.com/decklist/pauper-challenge-2020-05-3112162939"),        JsonFile="pauper-challenge-2020-05-3112162939.json" },
                 new Tournament(){ Date=new DateTime(2020, 05, 31, 00, 00, 00, DateTimeKind.Utc), Name="Legacy Challenge",        Uri=new Uri("https://www.mtgo.com/decklist/legacy-challenge-2020-05-3112162938"),        JsonFile="legacy-challenge-2020-05-3112162938.json" },
