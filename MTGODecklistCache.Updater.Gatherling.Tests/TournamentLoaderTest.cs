8c497b146314101afe509d380fc89c9142f77ea8
Filipe Badaró <badaro@gmail.com>
2024-12-08 19:17:45 -0300

    Starting gatherling scraper

---
+++ MTGODecklistCache.Updater.Gatherling.Tests/TournamentLoaderTest.cs
@@ -0,0 +1,83 @@
+using FluentAssertions;
+using MTGODecklistCache.Updater.Model;
+
+namespace MTGODecklistCache.Updater.Gatherling.Tests
+{
+    public class TournamentLoaderTest
+    {
+        CacheItem _tournament;
+
+        [SetUp]
+        public void Setup()
+        {
+            _tournament = new GatherlingSource().GetTournamentDetails(new Tournament()
+            {
+                Name = "The Penny Dreadful 500 (Season 35)",
+                Uri = new Uri("https://gatherling.com/eventreport.php?event=The%20Penny%20Dreadful%20500%20(Season%2035)"),
+                Date = new DateTime(2024, 11, 16, 13, 30, 00, DateTimeKind.Utc)
+            });
+        }
+
+        [Test]
+        public void ShouldLoadTournament()
+        {
+            _tournament.Should().NotBeNull();
+        }
+
+        [Test]
+        public void ShouldLoadCorrectTournamentInfo()
+        {
+            _tournament.Tournament.Name.Should().Be("The Penny Dreadful 500 (Season 35)");
+            _tournament.Tournament.Uri.Should().Be(new Uri("https://gatherling.com/eventreport.php?event=The%20Penny%20Dreadful%20500%20(Season%2035)"));
+            _tournament.Tournament.Date.Should().Be(new DateTime(2024, 11, 16, 13, 30, 00, DateTimeKind.Utc));
+        }
+
+        [Test]
+        public void ShouldGenerateJsonFile()
+        {
+            _tournament.Tournament.JsonFile.Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void ShouldGenerateJsonFileWithFormatForPennyDreadful()
+        {
+            _tournament.Tournament.JsonFile.Should().Contain("penny-dreadful");
+        }
+
+        [Test]
+        public void ShouldGenerateJsonFileWithFormatForPreModern()
+        {
+            CacheItem tournament = new GatherlingSource().GetTournamentDetails(new Tournament()
+            {
+                Name = "Pre-Modern Monthly League 14.06",
+            });
+
+            tournament.Tournament.JsonFile.Should().Contain("premod");
+            tournament.Tournament.JsonFile.Should().NotContain("modern");
+        }
+
+        [Test]
+        public void ShouldGenerateJsonFileWithFormatForPreFire()
+        {
+            CacheItem tournament = new GatherlingSource().GetTournamentDetails(new Tournament()
+            {
+                Name = "Pre-Fire Pop Ups 1.01",
+            });
+
+            tournament.Tournament.JsonFile.Should().Contain("prefire");
+            tournament.Tournament.JsonFile.Should().NotContain("modern");
+        }
+
+        [Test]
+        public void ShouldAddMissingFormatToJsonFile()
+        {
+            CacheItem tournament = new GatherlingSource().GetTournamentDetails(new Tournament()
+            {
+                Name = "2024 CPS Showdown November #3",
+            });
+
+            tournament.Tournament.JsonFile.Should().Contain("premod");
+            tournament.Tournament.JsonFile.Should().NotContain("modern");
+        }
+    }
+}
---
+++ MTGODecklistCache.Updater.Gatherling/GatherlingSource.cs
@@ -0,0 +1,80 @@
+﻿using MTGODecklistCache.Updater.Model;
+using MTGODecklistCache.Updater.Model.Sources;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace MTGODecklistCache.Updater.Gatherling
+{
+    public class GatherlingSource : ITournamentSource
+    {
+        public string Provider { get { return "gatherlink.com"; } }
+
+        private static string _eventPage = "https://gatherling.com/eventreport.php?event={eventName}";
+        private static string _eventPageApi = "https://gatherling.com/api.php?action=eventinfo&event={eventName}";
+        private static Dictionary<string, string> _validFormats = new Dictionary<string, string>()
+        {
+            { "Penny Dreadful", "penny dreadful" },
+            { "Pre-FIRE", "prefire" },
+            { "PreModern", "premod" }
+        };
+        private static Dictionary<string, string> _replacements = new Dictionary<string, string>()
+        {
+            { "Pre-FIRE", "prefire" },
+            { "Pre FIRE", "prefire" },
+            { "PreFIRE", "prefire" },
+            { "Pre Modern", "premod" },
+            { "Pre-Modern", "premod" },
+            { "PreModern", "premod" },
+            { "Penny Dreadful", "penny dreadful" },
+            { "Penny-Dreadful", "penny dreadful" },
+            { "PennyDreadful", "penny dreadful" },
+            { "Modern", "mod" }
+        };
+
+        public CacheItem GetTournamentDetails(Tournament tournament)
+        {
+            string eventPageApi = _eventPageApi.Replace("{eventName}", HttpUtility.UrlEncode(tournament.Name));
+            string jsonData = new WebClient().DownloadString(eventPageApi);
+
+            dynamic json = JsonConvert.DeserializeObject<dynamic>(jsonData);
+
+            var result = new CacheItem();
+
+            string eventName = json.name;
+            string eventDate = json.start;
+            string eventFormat = json.format;
+
+            result.Tournament = new Tournament();
+            result.Tournament.Name = eventName;
+            result.Tournament.Uri = new Uri(_eventPage.Replace("{eventName}", eventName));
+            result.Tournament.Date = DateTime.Parse(eventDate + "Z", CultureInfo.InvariantCulture).ToUniversalTime();
+            result.Tournament.JsonFile = GenerateFileName(result.Tournament, eventFormat);
+
+            return result;
+        }
+
+        public Tournament[] GetTournaments(DateTime startDate, DateTime? endDate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string GenerateFileName(Tournament tournament, string format)
+        {
+            string name = tournament.Name;
+
+            foreach (var replacement in _replacements)
+            {
+                if (name.Contains(replacement.Key, StringComparison.InvariantCultureIgnoreCase)) name = name.Replace(replacement.Key, replacement.Value, StringComparison.InvariantCultureIgnoreCase);
+            }
+
+            if (!name.Contains(_validFormats[format], StringComparison.InvariantCultureIgnoreCase)) name += $" ({_validFormats[format]})";
+
+            return $"{SlugGenerator.SlugGenerator.GenerateSlug(name.Trim())}-{tournament.Date.ToString("yyyy-MM-dd")}.json";
+        }
+    }
+}
